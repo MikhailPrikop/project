8a269269a7d02c37c5f85e2f1c0e625b310473f1
MikhailPrikop <prikop1@gmail.com>
2025-07-01 07:32:40 +0100

    Update ILINE.py

--- ILINE.py
+++ ILINE.py
@@ -7,183 +7,10 @@ config = configparser.ConfigParser()
 import datetime as dt
 import sys
 
-#from  functions import formation_request
-#from functions import formation_sql_reqursive
-#from functions import print_subordinates
-
-#пользовательские функции
-# функция формирования sql запросовpy
-def formation_request(expression, command='select', full_name_update=''):
-    def sql_simple(expression,
-                   sign,
-                   command,
-                   list_of_positions=
-                   ['CEO', 'MANAGER', 'TEAMLEAD', 'SENIORDEVELOPER', 'DEVELOPER']):
-        if expression.isalpha() == True:
-            if expression in list_of_positions:
-                if command != 'update':
-                    return (f" REPLACE(UPPER(position),' ', '') {sign} '{expression}' ")
-                else:
-                    return f"Обновить должность невозможно"
-            else:
-                if command != 'update':
-                    return (f" REPLACE(UPPER(full_name),' ', '') {sign} '{expression}' ")
-                else:
-                    return (f"full_name = '{expression}' ")
-
-        elif expression.isdigit() == True:
-            if ((len(expression) == 8)
-                    and (int(expression[0]) in [1, 2])
-                    and (int(expression[4:6]) <= 12)
-                    and (int(expression[6:]) <= 31)):
-                if command != 'update':
-                    return (f" date_employment {sign} '{expression}' ")
-                else:
-                    return (f"date_employment = '{expression}' ")
-            else:
-                if command != 'update':
-                    return (f" salary {sign} {expression} ")
-                else:
-                    return (f"salary = {expression} ")
-        else:
-            return ('')
-
-    expression = expression.replace(' ', '').upper()
-    sign = []
-    union = []
-    list_expression = []
-    sql_total = ""
-
-    # определение основной команды
-    if command == 'select':
-        sql_first_part = (f'SELECT full_name AS "ФИО", '
-                          f'position AS "Должность", '
-                          f'salary AS "Заработная плата", '
-                          f'date_employment AS "Дата принятия на работу" '
-                          f'FROM state_company '
-                          f'WHERE')
-        sql_end = ';'
-    elif command == 'delete':
-        sql_first_part = (f'DELETE '
-                          f'FROM state_company '
-                          f'WHERE ')
-        sql_end = ';'
-    elif command == 'update':
-        sql_first_part = (f'UPDATE state_company '
-                          f'SET ')
-        sql_end = (
-            f"WHERE REPLACE(UPPER(full_name),' ', '') = '{full_name_update.upper().replace(' ', '')}';")
-
-    # определенние операндов  и союзов
-    for i in expression:
-        if command == 'update':
-            union.append(',')
-        else:
-            if i == '|':
-                union.append('OR')
-                expression = expression.replace(i, '&')
-            elif i == '&':
-                union.append('AND')
-
-    list_expression = expression.split('&')
-    for i in range(len(list_expression)):
-        for j in list_expression[i]:
-            if j in ['<', '>', '=', '<=', '>=']:
-                list_expression[i] = list_expression[i].replace(j, '')
-                sign.append(j)
-            else:
-                sign.append("=")
-
-        # формирование запроса sql
-        if len(list_expression) == 1:
-            sql = sql_first_part + sql_simple(list_expression[0], sign[0], command=command) + sql_end
-        else:
-            for i in range(len(list_expression) - 1):
-                sql_second_part = sql_simple(list_expression[i], sign[i], command=command) + union[i]
-            sql_total = sql_total + sql_second_part
-        sql = sql_first_part + sql_total + sql_simple(list_expression[-1], sign[-1], command=command) + sql_end
-        return sql
-
-
-def formation_sql_reqursive(name, goal_sign):
-    # запрос формирование таблицы подчиненных
-    if goal_sign == '-':
-        sql = (
-            f"WITH RECURSIVE cte_subordinates AS ("
-            f"SELECT 1 as Level, position, full_name, id, parent_id, CAST(id AS TEXT) AS path "
-            f"FROM state_company "
-            f"WHERE REPLACE(UPPER(full_name),' ','') = '{name}' "
-            f"UNION ALL "
-            f"SELECT level +1, sc.position, sc.full_name, sc.id, sc.parent_id, cs.Path || '->' || sc.id "
-            f"FROM state_company sc "
-            f"JOIN cte_subordinates cs ON sc.parent_id = cs.id) "
-            f"SELECT  level, position, full_name, path "
-            f"FROM cte_subordinates "
-            f"ORDER BY path"
-        )
-    elif goal_sign == '0':
-        sql = (
-            f"WITH RECURSIVE cte_subordinates AS ("
-            f"SELECT 1 as Level, position, full_name, id, parent_id, CAST(id AS TEXT) AS path "
-            f"FROM state_company "
-            f"WHERE  parent_id is null "
-            f"UNION ALL "
-            f"SELECT level +1, sc.position, sc.full_name, sc.id, sc.parent_id, cs.Path || '->' || sc.id "
-            f"FROM state_company sc "
-            f"JOIN cte_subordinates cs ON sc.parent_id = cs.id) "
-            f"SELECT  level, position, full_name, path "
-            f"FROM cte_subordinates "
-            f"WHERE level+1 "
-        )
-    # запрос на формирование таблицы начальства
-    else:
-        sql = (
-            f"WITH RECURSIVE cte_subordinates AS ("
-            f"SELECT 1 as Level, position, full_name, id, parent_id, CAST(position AS TEXT) AS path_position, "
-            f"CAST(full_name AS TEXT) AS path_name "
-            f"FROM state_company "
-            f"WHERE  parent_id is null "
-            f"UNION ALL "
-            f"SELECT level +1, sc.position, sc.full_name, sc.id, sc.parent_id, cs.path_position || ' -> ' || sc.position, "
-            f"cs.path_name || ' -> ' || sc.full_name "
-            f"FROM state_company sc "
-            f"JOIN cte_subordinates cs ON sc.parent_id = cs.id) "
-            f"SELECT id, path_position, path_name "
-            f"FROM cte_subordinates "
-            f"WHERE REPLACE(UPPER(full_name),' ','') = '{name}' "
-        )
-    return sql
-
-# функция печати лерева субординации
-def print_subordinates(sql_rows, goal_sign, level_subardinates=5):
-    if goal_sign == '-':
-        max_len = len(','.join(max(sql_rows)[1:3])) + level_subardinates * 4
-        print(f" {'=' * max_len}")
-        print(f" {sql_rows[0][1]} ({sql_rows[0][2]})")
-        print(f" {'=' * max_len}")
-        for i in range(1, len(sql_rows) - 1):
-            if len(sql_rows[i][3]) <= len(sql_rows[i + 1][3]):
-                print(f"{' | ' * (sql_rows[i][0] - 2)} |-- {sql_rows[i][1]} ({sql_rows[i][2]})")
-            else:
-                print(f"{' | ' * (sql_rows[i][0] - 2)} └── {sql_rows[i][1]} ({sql_rows[i][2]})")
-                print(f"{' | ' * (sql_rows[i][0] - 2)}")
-        print(
-            f"{' | ' * (sql_rows[len(sql_rows) - 1][0] - 2)} └── {sql_rows[len(sql_rows) - 1][1]} ({sql_rows[len(sql_rows) - 1][2]})")
-        print(f" {'=' * max_len}")
-    else:
-        lst_positions = sql_rows[0][1].split(' -> ')
-        lst_name = sql_rows[0][2].split(' -> ')
-        print(f"{'Должность':^50} {'ФИО':^50}")
-        for i in range(len(lst_positions) - 1):
-            print(f"{'=' * 100}")
-            print(f"{lst_positions[i]:^50}|{lst_name[i]:^50}")
-            print(f"{'=' * 100}")
-            print(f" ")
-            print(f"{'↓':^50}|{'↓':^50}")
-            print(f" ")
-        print(f"{'=' * 100}")
-        print(f"{lst_positions[len(lst_positions) - 1]:^50}|{lst_name[len(lst_positions) - 1]:^50}")
-        print(f"{'=' * 100}")
+from  functions import formation_request
+from functions import formation_sql_reqursive
+from functions import print_subordinates
+from functions import create_table_test
 
 #Основной код
 #Эмблема приветствие
@@ -304,6 +131,7 @@ if connect_db == True:
     print("- для отображения иерархии сотрудников введите I")
     print("- для удаления информации из базы данных нажмите клавишу '-'")
     print("- для выхода из программы введите 'ВЫХОД' или 'EXIT'")
+    print("- для формирования фиктивной тестовой базы данных о сотрудниках введите 'TEST'")
     print("- для вызова памятки введите ?")
     print(f"{'*' * 120}")
     print(" ")
@@ -511,6 +339,7 @@ if connect_db == True:
             print('- для обновления информации в базе данных нажмите клавишу "R"')
             print("- для добавления информации в базу данных  нажмите клавишу '+'")
             print("- для удаления информации из базы данных нажмите клавишу '-'")
+            print("- для формирования фиктивной тестовой базы данных о сотрудниках введите 'TEST'")
             print("- для вызова памятки введите ?")
             print(f"{'-' * 120}")
             print(" ")
@@ -554,20 +383,45 @@ if connect_db == True:
         elif user_input.upper() == 'EXIT' or user_input.upper() == 'ВЫХОД':
             break
         # поиск и фильтрация данных
-        else:
-            sql = formation_request(user_input)
+        elif user_input.upper() == 'TEST':
+            create_table_test()
+            path_csv = os.path.abspath('state_company.csv')
+            sql = (f"CREATE TABLE state_company ( "
+            f"id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,"
+            f"full_name VARCHAR(200), "
+            f"position VARCHAR(200) NOT NULL, "
+            f"date_employment DATE, "
+            f"salary INT, "
+            f"parent_id INT, "
+            f"CONSTRAINT state_company_parent_id_fk FOREIGN KEY(parent_id) "
+            f"REFERENCES state_company(id) ON DELETE CASCADE "
+            f");"
+    
+            f"COPY state_company(full_name, position, date_employment, salary, parent_id) "
+            f"FROM '{path_csv}' DELIMITER ',' CSV HEADER; "
+            f"SELECT * FROM state_company " )
+            print(path_csv)
             try:
                 cur.execute(sql)
                 sql_rows = cur.fetchall()
-                if sql_rows != []:
-                    print(tabulate(sql_rows,
-                                   headers=['ФИО сотрудника', 'Должность', 'Заработная плата', 'Дата принятия на работу'],
-                                   tablefmt='grid'))
-                else:
-                    print("ИНФОРМАЦИЯ НЕ НАЙДЕНА")
             except:
-                print("ОШИБКА!!! ВОЗМОЖНО ВВЕДЕНЫ НЕДОПУСТИМЫЕ СИМВОЛЫ")
+                print("СФОРМИРОВАТЬ ТЕСТОВУЮ ТАБЛИЦУ НЕ УДАЛОСь")
 
-    cur.close()
-    conn.close()
-    sys.exit()
+
+        else:
+                sql = formation_request(user_input)
+                try:
+                    cur.execute(sql)
+                    sql_rows = cur.fetchall()
+                    if sql_rows != []:
+                        print(tabulate(sql_rows,
+                                       headers=['ФИО сотрудника', 'Должность', 'Заработная плата', 'Дата принятия на работу'],
+                                       tablefmt='grid'))
+                    else:
+                        print("ИНФОРМАЦИЯ НЕ НАЙДЕНА")
+                except:
+                    print("ОШИБКА!!! ВОЗМОЖНО ВВЕДЕНЫ НЕДОПУСТИМЫЕ СИМВОЛЫ")
+
+        cur.close()
+        conn.close()
+        sys.exit()
